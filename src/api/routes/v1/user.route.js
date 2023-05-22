dd4c5de43767d249dd558a7e19e0e373892cbdcb
Tamir Lahav <tamirla@gmail.com>
2023-05-22 13:20:15 +0300

    add cancel notification api

--- src/api/routes/v1/user.route.js
+++ src/api/routes/v1/user.route.js
@@ -190,5 +190,6 @@ router.route('/updateProfile/:userId').post(authorize(LOGGED_USER), controller.u
 router.route('/changePassword/:userId').post(authorize(LOGGED_USER), controller.changePassword);
 router.route('/cancelSubsciption/:userId').post(authorize(LOGGED_USER), controller.sendCancelSubscriptionEmail);
 router.route('/saveNotification/:userId').post(authorize(LOGGED_USER), controller.saveNotification);
+router.route('/cancelNotification/:userId').post(authorize(LOGGED_USER), controller.cancelNotification);
 
 module.exports = router;
